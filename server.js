92dee0a691c4277c19db52acb0cae8675a253374
Glyn L <glyn_92@hotmail.com>
2018-05-28 15:47:53 +0700

    mvp

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var upload = multer();
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res) {
+  res.json({ filename: req.file.originalname, size: req.file.size });
+});
+
+app.get("/hello", function(req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
 });
